025b1dd5222d4bc751b0bcca523594ef28b581b5
Lloyd Sparkes <lloyd@lloydsparkes.co.uk>
2016-01-23 10:48:09 +0000

    Part 3

---
+++ src/NzbDrone.Common/Http/HttpRequestProxySettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NzbDrone.Common.Http
+{
+    public enum ProxyType
+    {
+        Http,
+        Socks4,
+        Socks5
+    }
+
+    public class HttpRequestProxySettings
+    {
+        public HttpRequestProxySettings(ProxyType type, string host, int port, string username = null, string password = null)
+        {
+            Type = type;
+            Host = host;
+            Port = port;
+            Username = username;
+            Password = password;
+        }
+
+        public ProxyType Type { get; private set; }
+        public string Host { get; private set; }
+        public int Port { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+    }
+}
---
+++ src/NzbDrone.Core/Http/ProxyHttpInterceptor.cs
@@ -0,0 +1,39 @@
+﻿using NzbDrone.Common.Http;
+using NzbDrone.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NzbDrone.Core.Http
+{
+    public class ProxyHttpInterceptor : IHttpRequestInterceptor
+    {
+        private readonly IConfigService _configService;
+
+        public ProxyHttpInterceptor(IConfigService configService)
+        {
+            this._configService = configService;
+        }
+
+        public HttpResponse PostResponse(HttpResponse response)
+        {
+            return response;
+        }
+
+        public HttpRequest PreRequest(HttpRequest request)
+        {
+            if(_configService.ProxyEnabled == UseProxy.Global ||
+                (_configService.ProxyEnabled == UseProxy.IndexersOnly && request.IsIndexerRequest))
+            {
+                request.Proxy = new HttpRequestProxySettings(_configService.ProxyType,
+                    _configService.ProxyHostname,
+                    _configService.ProxyPort,
+                    _configService.ProxyUsername == string.Empty ? null : _configService.ProxyUsername,
+                    _configService.ProxyPassword == string.Empty ? null : _configService.ProxyPassword);
+            }
+
+            return request;
+        }
+    }
+}
